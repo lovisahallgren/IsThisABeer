6c9829e9949dd1a467ad136a476b877332866ca4
Izabella <izzwii.izabella@gmail.com>
2019-02-18 15:31:12 +0100

    added the api key

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import app from './app';
-
 import '../styles/index.scss';
+
+const apiKey =
+  "?client_id=e3b87f06abb975fa7328d3cdebb73fbdce090e5581320fcbd497303e52ed5f40";
+const api = "https://api.unsplash.com/photos/random";
+let query = "&query=water";
+const url = api + apiKey + query;
+
